f9a789c7c538681668e76ac2650608349bafd90a
josip-volarevic <josip.volarevic4@gmail.com>
2022-11-03 13:47:44 +0100

    chore: update metadata images

--- pages/comic.tsx
+++ pages/comic.tsx
@@ -1,7 +1,4 @@
 import type { NextPage } from 'next'
-import Navigation from 'components/layout/Navigation'
-import Footer from 'components/layout/Footer'
-import Main from 'components/layout/Main'
 import NikoComic from 'components/NikoComic'
 
 const Comic: NextPage = () => {
